2e8865b9b61de6adbc985f3706ee38b2c5532d7b
Vladimir <vovacomua@bk.ru>
2018-02-15 19:50:02 +0300

    27 Markdown Mail

---
+++ resources/views/emails/welcome-again.blade.php
@@ -0,0 +1,17 @@
+@component('mail::message')
+# Introduction
+
+The body of your message.
+
+- one
+- two
+- three
+
+
+@component('mail::button', ['url' => ''])
+Button Text
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
